e3eb88b1ae2c48f25eba3fa45dabd349ddca4923
Josh Nussbaum <joshnuss@gmail.com>
2020-02-27 22:38:22 -0500

    Adds preprocessor

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,127 @@ import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 
+import postcss from 'postcss'
+import tailwind from 'tailwindcss'
+
+import { parseFragment, serialize } from 'parse5'
+
+function getSelectors() {
+  return postcss(tailwind())
+    .process("@tailwind utilities;")
+    .then(result => {
+      const {nodes} = result.root
+      const rules = []
+
+      nodes.forEach(node => {
+        if (node.type === 'rule') {
+          rules.push(node)
+        } else if (node.type === 'atrule' && node.name === 'media') {
+          node.nodes.forEach(x => rules.push(x))
+        }
+      })
+
+      return rules
+        .filter(n => n.selector)
+        .map(n => {
+          return n.selector
+            .replace(/^\./, '')
+            .replace('\\', '')
+            .replace(/\:focus$/, '')
+            .replace(/\:hover$/, '')
+        })
+    })
+}
+
+function replace(nodes, selectors) {
+  return nodes.map(node => {
+    if (node.attrs == []) return
+    if (node.nodeName == '#text') return
+
+    const {classes, others} = extract(node.attrs, selectors)
+
+    node.attrs = others
+
+    if (classes.length > 0) {
+      let cssClass = ''
+
+      classes.forEach(({type,  attr}) => {
+        if (type === 'dynamic') {
+          const expression = attr.value.split(/\{|\}/)[1]
+          cssClass += `{${expression} ? ' ${attr.name}' : ''} `
+        } else {
+          cssClass += attr.name + ' '
+        }
+      })
+
+      node.attrs.push({name: 'class', value: cssClass.trim()})
+    }
+
+    if (node.childNodes) {
+      replace(node.childNodes, selectors)
+    }
+
+    return node
+  })
+}
+
+function extract(attrs, selectors) {
+  const others = []
+  const classes = []
+
+  attrs.forEach(attr => {
+    const {name, value} = attr
+    let names
+    const parts = name.split(':')
+    let modifier = parts.length > 1 ? parts[0] : null
+
+    if (name.indexOf(',') > -1) {
+      if (modifier) {
+        names = parts[1].split(',')
+      }
+      else {
+        names = parts[0].split(',')
+      }
+    }
+    else {
+      modifier = null
+      names = [name]
+    }
+
+    names.forEach(name => {
+      const fullName = modifier ? `${modifier}:${name}` : name
+      const fullAttr = {name: fullName, value}
+
+      if (selectors.indexOf(fullName) > -1) {
+        if (!value) {
+          classes.push({type: 'static', attr: fullAttr})
+        } else {
+          classes.push({type: 'dynamic', attr: fullAttr})
+        }
+      } else {
+        others.push(fullAttr)
+      }
+    })
+  })
+
+  return {others, classes}
+}
+
+function preprocessTailwind({content}) {
+  const doc = parseFragment(content)
+
+  console.log(doc.childNodes[2])
+  console.log(serialize(doc))
+
+  return getSelectors().then(selectors => {
+    replace(doc.childNodes, selectors)
+
+    console.log(serialize(doc))
+
+    return {code: serialize(doc)}
+  })
+}
+
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
@@ -16,6 +137,10 @@ export default {
   },
   plugins: [
     svelte({
+      preprocess: {
+        markup: preprocessTailwind
+      },
+
       // enable run-time checks when not in production
       dev: !production,
       // we'll extract any component CSS out into
--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,6 @@
-<h1 class="text-4xl">Hello World</h1>
+<button bg-red-200,m-4 md:bg-red-400,m-8 text-4xl,font-bold={true} md:text-5xl p-4 hover:text-red-600,bg-blue-200>
+  {#if true}Hello <span font-bold>World</span>!{/if}
+</button>
+
+<h1 text-4xl (bg-red-200,font-bold)>Hello World!</h1>
+<p font-normal>Bla</p>
